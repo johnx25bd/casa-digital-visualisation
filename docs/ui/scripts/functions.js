e23deded8449b25da9e3ca7f218a75824fc68726
John IV <john.r.hoopes@gmail.com>
2019-05-09 13:56:27 +0100

    Progress on card-map animation, plus automated layer load process

---
+++ docs/ui/scripts/functions.js
@@ -0,0 +1,131 @@
+
+function buildAddLayerParams(_layerData) {
+  var outputParams = {};
+  outputParams.id = _layerData.name;
+  outputParams.source = _layerData.name + '-source';
+  outputParams = {...outputParams, ... _layerData.addLayerParams};
+  console.log('Output Params', outputParams);
+  return outputParams;
+}
+
+
+function isElementOnScreen(cardNum) {
+  // Directly from https://docs.mapbox.com/mapbox-gl-js/example/scroll-fly-to/
+  var element = document.querySelector("div[data-index='" + String(cardNum) + "']")
+  var bounds = element.getBoundingClientRect();
+
+  // !!! This could use some work - improve moment when
+  // new active card is set ... could add clicked? attribute,
+  // and
+  return bounds.top < window.innerHeight && bounds.bottom > 80;
+}
+
+
+function loadCards(cards) {
+  // iterate through and load cards into .cards div
+
+  // cards.forEach(function (cardData) {
+  //
+  //
+  //   console.log(cardData)
+  // });
+  console.log(cards);
+
+  var cardsHolder = d3.select('#app-cards');
+
+
+  var cardEls = cardsHolder.selectAll('div')
+    .data(cards).enter()
+    .append('div')
+    .attr('id', function(d) {
+      return d.extent + "-card-" + String(d.cardNum);
+    })
+    .attr('data-index', function(d, i) {
+      return i;
+    })
+    .attr('class', function(d) {
+      return d.extent;
+    })
+    .classed('col-12', true)
+    .classed('app-card', true)
+    .on('click', function(d) {
+      console.log("CLICK!");
+      setActiveCard(d.cardNum);
+    });
+
+  cardEls.append('h1')
+    .text(function(d) {
+      return d.title;
+    });
+
+  cardEls.append('div')
+    .classed('card-body', true)
+    .html(function(d) {
+      return d.content;
+    })
+
+}
+
+function showCardLayers(cardNum) {
+
+  var layers = cardData[cardNum].layers;
+
+  Object.keys(loadedData).forEach(function(layer) {
+
+    if (layers.includes(layer)) {
+      map.setLayoutProperty(layer, 'visibility', 'visible');
+    } else {
+      map.setLayoutProperty(layer, 'visibility', 'none');
+    }
+  });
+
+  updateLegend(cardNum)
+}
+
+
+function updateLegend(layers) {
+  // iterate through array of layers
+  // Add each layer's legend to .legend div
+  // console.log("updateLegend() Called");
+}
+
+
+function setActiveCard(cardNum) {
+  if (cardNum === activeCardNum) {
+    return;
+  }
+  //
+
+  scrollToCard(cardNum);
+  // console.log(cardData[cardNum]);
+  map.flyTo(cardData[cardNum].flyTo);
+
+  $("div[data-index='" + String(cardNum) + "']")
+    .addClass('active');
+  $("div[data-index='" + String(activeCardNum) + "']")
+    .removeClass('active');
+
+  // consider scroll .cards div to card ...
+
+  showCardLayers(cardNum);
+
+  activeCardNum = cardNum;
+// }
+}
+
+function scrollToCard(cardNum) {
+  // adapted from https://stackoverflow.com/questions/6677035/jquery-scroll-to-element
+
+  inAnimation = true;
+  // console.log("inAnimation:", inAnimation);
+  var cardTmp = cardData[cardNum];
+  var id = '#'  + cardTmp.extent + '-card-' + String(cardNum);
+  // console.log(id);
+  // setActiveCard(cardNum);
+
+  $([document.documentElement, document.body]).animate({
+      scrollTop: $(id).offset().top - 80
+  }, 500, function () {
+    inAnimation = false; });
+}
+// $("#button").click(function() {
--- docs/ui/scripts/load-map.js
+++ docs/ui/scripts/load-map.js
@@ -1,12 +1,5 @@
-// var mapLeaflet = L.map('L-map').setView([51,505, -0.09], 13)
-//
-// L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-//     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-//     maxZoom: 18,
-//     id: 'mapbox.streets',
-//     accessToken: 'pk.eyJ1Ijoicm9iaXNvbml2IiwiYSI6ImNqbjM5eXEwdjAyMnozcW9jMzdpbGk5emoifQ.Q_S2qL8UW-UyVLikG_KqQA'
-// }).addTo(mapLeaflet);
-
+// SOURCES
+// https://docs.mapbox.com/mapbox-gl-js/example/scroll-fly-to/
 
 // Globals
 
@@ -15,7 +8,10 @@ var dims = {
   w: window.innerWidth
 }
 
+var activeCardNum = null;
+var inAnimation = false;
 
+// Instantiate map:
 mapboxgl.accessToken = 'pk.eyJ1Ijoicm9iaXNvbml2IiwiYSI6ImNqbjM5eXEwdjAyMnozcW9jMzdpbGk5emoifQ.Q_S2qL8UW-UyVLikG_KqQA';
 var map = new mapboxgl.Map({
   container: 'main-map', // container id
@@ -24,147 +20,130 @@ var map = new mapboxgl.Map({
   zoom: 2 // starting zoom
 });
 
-// var map = new mapboxgl.Map({
-//   container: "main-map",
-//   center: [37.8, -96.9],
-//   zoom: 2
-// });
-
-// .addLayer(new L.TileLayer("http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"));
-
+var cardData = [],
+  loadedData = {};
 
 // LOAD LAYERS
 
 // Load and organize all data
-var dataFiles = ["./data/us-states.json", "./data/airports.json", "./data/countries.json", "./data/cards.json"],
-  dataPromises = [],
-  loadedData = {};
+d3.json('./data/layers.json')
+  .then(function(layersData) {
+    console.log(layersData);
 
-dataFiles.forEach(function(filename) {
+    var dataPromises = [];
 
-  var filetype = filename.split('.'),
-    filetype = filetype[filetype.length - 1];
-
-  if (filetype == "csv") {
-    dataPromises.push(d3.csv(filename));
-  } else if (filetype == "json") {
-    dataPromises.push(d3.json(filename));
-  } else {
-    console.log("Please pass a valid file to load.");
-  }
-});
-
-Promise.all(dataPromises)
-  .then(function(values) {
-
-    for (var i = 0; i < values.length; i++) {
-      filename = dataFiles[i];
-
+    layersData.forEach(function(layerData) {
+      var filename = layerData.path;
       var filetype = filename.split('.'),
-        filekey = filetype[1].split('/');
-
-      filekey = filekey[filekey.length - 1]; // should be file name ...
-      filetype = filetype[filetype.length - 1];
-
-      // possibly - have to solve typecasting to numbers for each file ...
-      // if (filekey == 'airports') {
-      //
-      //   console.log(values[i].map(x => x.lon += x.lon; ))
-      //
-      //   //values[i] = values[i]
-      // }
-      loadedData[filekey] = {
-        filetype: filetype,
-        data: values[i]
-      };
-    }
-
-    // Once all data is loaded, add to map
-    map.on('load', function() {
-
-      map.addSource("us-states", {
-        "type": "geojson",
-        "data": loadedData['us-states'].data
-      });
-
-      map.addSource("airports", {
-        "type": "geojson",
-        "data": loadedData['airports'].data
-      });
-
-      map.addSource("countries", {
-        "type": "geojson",
-        "data": loadedData['countries'].data
-      });
+        filetype = filetype[filetype.length - 1];
 
 
-      map.addLayer({
-        "id": "us-states-polys",
-        "type": "fill",
-        "source": "us-states",
-        'layout': {},
-        'paint': {
-          'fill-color': '#088',
-          'fill-opacity': 0.8
-        }
-      });
 
-      // map.addLayer({
-      //   "id": "countries-polys",
-      //   "type": "fill",
-      //   "source": "countries",
-      //   'layout': {},
-      //   'paint': {
-      //     'fill-color': '#088',
-      //     'fill-opacity': 0.8
-      //   }
-      // });
-
-      map.addLayer({
-        "id": "points",
-        "type": "circle",
-        "source": "airports",
-        "paint": {
-          "circle-radius": [
-            'match',
-            ['get', 'type'],
-            'small', 3,
-            'mid', 5,
-            'major', 9,
-            // 'Hispanic', '#e55e5e',
-            // 'Asian', '#3bb2d0',
-            /* other */
-            2
-          ],
-          "circle-color": [
-            'match',
-            ['get', 'type'],
-            'small', '#fbb03b',
-            'mid', '#223b53',
-            'major', 'yellow',
-            // 'Hispanic', '#e55e5e',
-            // 'Asian', '#3bb2d0',
-            /* other */
-            '#ccc'
-          ]
-
-        }
-
-
-      });
-
-      // LOAD CARDS
-      loadCards(loadedData["cards"].data);
-      //
-
-      setActiveCard(0);
-
+      if (filetype == "csv") {
+        dataPromises.push(d3.csv(filename));
+      } else if (filetype == "json") {
+        dataPromises.push(d3.json(filename));
+      } else {
+        console.log("Error with file", filename, ". Please pass a valid file to load.");
+      }
     });
 
 
-  }).catch(function(e) {
+
+    Promise.all(dataPromises)
+      .then(function(values) {
+
+        for (var i = 0; i < values.length; i++) {
+          filename = layersData[i].path;
+          // console.log("in promise", filename);
+
+          // console.log('layerdata', layerData);
+          if (filename.includes('cards')) {
+            cardData = values[i];
+            console.log("includes cards", values[i]);
+          } else {
+            var filetype = layersData[i].path.split('.'),
+              filekey = filetype[1].split('/');
+
+            filekey = filekey[filekey.length - 1]; // should be file name ...
+            filetype = filetype[filetype.length - 1];
+
+            loadedData[filekey] = {
+              filetype: filetype,
+              data: values[i]
+            };
+          }
+        }
+
+        layersData = layersData.slice(1, layersData.length);
+
+
+        // Once all data is loaded, add to map
+        map.on('load', function() {
+
+          console.log('ld', buildAddLayerParams(layersData));
+
+          layersData.forEach(function (layerData) {
+            var dataKey = layerData.name;
+            map.addSource(dataKey + '-source', {
+              "type": "geojson",
+              "data": loadedData[dataKey].data
+            });
+            map.addLayer(buildAddLayerParams(layerData));
+          });
+
+          // LOAD CARDS
+          loadCards(cardData);
+          setActiveCard(0);
+        });
+      }).catch(function(e) {
+        console.log(e);
+      });
+  })
+  .catch(function(e) {
     console.log(e);
   });
+
+
+
+// EVENT LISTENERS
+window.onscroll = function() {
+  for (var i = 0; i < cardData.length; i++) {
+    if (isElementOnScreen(i) && inAnimation == false) {
+      setActiveCard(i);
+      break;
+    }
+  }
+};
+
+// $('#mode-buttons').on('click', function (e) {
+//   if this.hasClass('active') {
+//     return;
+//   } else {
+//
+//   }
+//   console.log("Button clicked", e);
+//   console.log(this);
+// } )
+
+$('#next-card').on('click', function (e) {
+  e.preventDefault();
+  if (activeCardNum < cardData.length) {
+    setActiveCard(activeCardNum + 1);
+  }
+
+})
+
+$('#previous-card').on('click', function (e) {
+  e.preventDefault();
+  console.log("Previous", activeCardNum);
+  if (activeCardNum > 0) {
+    setActiveCard(activeCardNum - 1);
+    scrollToCard(cardNum);
+
+  }
+
+})
 
 
 
@@ -183,103 +162,4 @@ flyTo: {
     zoom: 15.5,
     pitch: 20 // for 3d effect
   },
-
-
-
 */
-
-var activeCardNum = null;
-
-function setActiveCard(cardNum) {
-  if (cardNum === activeCardNum) return;
-
-  // console.log(loadedData['cards'].data[cardNum]);
-  map.flyTo(loadedData['cards'].data[cardNum].flyTo);
-
-  $("div[data-index='" + String(cardNum) + "']")
-    .addClass('active');
-
-  $("div[data-index='" + String(activeCardNum) + "']")
-    .removeClass('active');
-
-  // document.getElementById(chapterName).setAttribute('class', 'active');
-  // document.getElementById(activeChapterName).setAttribute('class', '');
-
-  showCardLayers(cardNum);
-  //
-  activeCardNum = cardNum;
-}
-
-
-function showCardLayers(cardNum) {
-
-  updateLegend(cardNum)
-}
-
-
-function updateLegend(layers) {
-  // iterate through array of layers
-  // Add each layer's legend to .legend div
-}
-
-
-function loadCards(cards) {
-  // iterate through and load cards into .cards div
-
-  // cards.forEach(function (cardData) {
-  //
-  //
-  //   console.log(cardData)
-  // });
-  console.log(cards);
-
-  var cardsEls = d3.select('#app-cards');
-
-
-  cardsEls.selectAll('div')
-    .data(cards).enter()
-    .append('div')
-    .attr('id', function(d) {
-      return d.extent + "-card-" + String(d.cardNum);
-    })
-    .attr('data-index', function(d, i) {
-      return i;
-    })
-    .attr('class', function(d) {
-      return d.extent;
-    })
-    .classed('col-12', true)
-    .classed('app-card', true)
-
-    .append('h1')
-    .text(function(d) {
-      return d.title;
-    })
-    .on('click', function (d) {
-      console.log("CLICK!");
-      setActiveCard(d.cardNum);
-    })
-
-}
-
-
-function isElementOnScreen(cardNum) {
-  // Directly from https://docs.mapbox.com/mapbox-gl-js/example/scroll-fly-to/
-  var element = document.querySelector("div[data-index='" + String(cardNum) + "']")
-  var bounds = element.getBoundingClientRect();
-
-  // !!! This could use some work - improve
-  return bounds.top < window.innerHeight && bounds.bottom > 50;
-}
-
-window.onscroll = function() {
-  // var chapterNames = Object.keys(chapters);
-  for (var i = 0; i < loadedData.cards.data.length; i++) {
-    // console.log(i);
-    // var chapterName = chapterNames[i];
-    if (isElementOnScreen(i)) {
-      setActiveCard(i);
-      break;
-    }
-  }
-};
