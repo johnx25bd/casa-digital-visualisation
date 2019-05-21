670ce63be14f10b79076887ba0f6af46c3aaa442
Kristian Lunow Nielsen <KristianLN92@gmail.com>
2019-05-21 14:11:29 +0100

    Testing

--- docs/testKristian/scripts/functions.js
+++ docs/testKristian/scripts/functions.js
@@ -102,7 +102,7 @@ function loadCards(_cards) {
       card.loadCard(i, card);
     }
   }
-  console.log(_cards[0].layers)
+  console.log('Layers',_cards[0].layers)
 }
 
 function showCardLayers(_cardNum) {
