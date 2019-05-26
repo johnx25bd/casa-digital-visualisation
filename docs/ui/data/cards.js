dd10552cc2ad5e2ceb16e7695f38fe3eadeaed41
Kristian Lunow Nielsen <KristianLN92@gmail.com>
2019-05-26 18:12:09 +0100

    nothing important

--- docs/ui/data/cards.js
+++ docs/ui/data/cards.js
@@ -200,7 +200,7 @@ var cardData = [
         <iframe width="100%" height="100" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/553784094&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe>
     `,
 
-    "layers": ['arcTestLayer'],//'arcTestLayer'
+    "layers": ['my-arc'],//'arcTestLayer'
     "flyTo": {
       "bearing": 0,
       "center": {"lng":-0.7973131555806958,"lat":34.84825706688895},
