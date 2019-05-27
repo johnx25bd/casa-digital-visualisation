bdd9b05894a5cf5401f213ee51e0023edafbe13e
John IV <john.r.hoopes@gmail.com>
2019-05-27 12:52:12 +0100

    Update to index.html and heatmap color ramp

--- docs/ui/data/layers.js
+++ docs/ui/data/layers.js
@@ -360,10 +360,10 @@ var layersData = [{
             ["linear"],
             ["heatmap-density"],
             0, "rgba(33,102,172,0)",
-            // 0.2, "rgb(103,169,207)",
-            // 0.4, "rgb(209,229,240)",
-            // 0.6, "rgb(253,219,199)",
-            // 0.8, "rgb(239,138,98)",
+            0.2, "rgb(103,169,207)",
+            0.4, "lightgreen",
+            0.6, "rgb(253,219,199)",
+            0.8, "rgb(239,138,98)",
             1, "rgb(178,24,43)"
           ],
           // Adjust the heatmap radius by zoom level
